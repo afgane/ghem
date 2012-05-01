8910c5602ed9e4f263341ae12b964830209ea09a
Enis Afgan <afgane@gmail.com>
2012-05-01 22:09:08 +1000

    Add DRMAA_LIBRARY_PATH to env before starting the webapp

--- ghem/manage.py
+++ ghem/manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ghem.settings")
+    os.environ.setdefault("DRMAA_LIBRARY_PATH", "/opt/sge/lib/lx24-amd64/libdrmaa.so.1.0")
 
     from django.core.management import execute_from_command_line
 
