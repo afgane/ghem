f7e3bf1ae10526c2295a4ecdf08f4884f63de146
Enis Afgan <afgane@gmail.com>
2012-06-03 12:22:29 +1000

    At service start, update any new libs that might have been added to requirements.txt

--- ghem/run_gunicorn.sh
+++ ghem/run_gunicorn.sh
@@ -13,11 +13,15 @@ export DRMAA_LIBRARY_PATH=/opt/sge/lib/lx24-amd64/libdrmaa.so.1.0
 cd /home/ubuntu/weather/ghem
 # Always update the code from the repo on service start
 git pull
-# Activate the virtual env and start the app as a wsgi app
+# Activate the virtual env
 source ../bin/activate
+# Update any new libs that might have been added to requirements.txt
+pip install -r requirements.txt
+# Setup the log file & permissions
 test -d $LOGDIR || mkdir -p $LOGDIR
-touch $LOGFILE
+test -e $LOGFILE || touch $LOGFILE
 chown -R $USER:$USER $LOGDIR
+# Start the web app as a gunicorn-managed wsgi app
 exec ../bin/gunicorn ghem.wsgi:application \
   -w $NUM_WORKERS \
   --user=$USER --group=$GROUP --log-level=debug \
