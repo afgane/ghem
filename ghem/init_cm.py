9f811a1c87f2d1b3b5517d86766c1f4a4f02a681
Enis Afgan <afgane@gmail.com>
2012-08-25 23:09:54 +1000

    Initialize CloudMan only on the master instance

--- ghem/init_cm.py
+++ ghem/init_cm.py
@@ -21,21 +21,28 @@ try:
     ud_file = '/tmp/cm/userData.yaml'
     with open(ud_file, 'r') as f:
         ud = yaml.load(f)
-    role = ud.get('role', 'master')
+    role = ud.get('role', '')
     cm_pwd = ud.get('password', '')
 except Exception, e:
     log.error("Error reading user data file {0}: {1}".format(ud_file, e))
     sys.exit(1)
-# Initialize CloudMan if not already initialized
-cm = CloudMan('http://127.0.0.1:42284/', cm_pwd)
-cm_type = cm.get_cluster_type()
-log.debug("\nCurrent CloudMan type: '{0}'".format(cm_type))
-if cm_type == '' or cm_type is None:
-    log.debug("Initializing CloudMan to type 'SGE'")
-    cm.initialize(type='SGE')
-    log.debug("Scaling the size of CloudMan cluster")
-    # Enable autoscaling with sufficient cluster size limits to enable the
-    # 22 models to run in parallel (assuming Large instance types w/ 4 CPUs)
-    cm.enable_autoscaling(0, 7)
+# Initialize CM only on the master node. This assumes the master is:
+#  1. Running jobs
+#  2. Runs the very first job
+if role == 'master':
+    # Initialize CloudMan if not already initialized
+    cm = CloudMan('http://127.0.0.1:42284/', cm_pwd)
+    cm_type = cm.get_cluster_type()
+    log.debug("Current CloudMan type: '{0}'".format(cm_type))
+    if cm_type == '' or cm_type is None:
+        log.debug("Initializing CloudMan to type 'SGE'")
+        cm.initialize(type='SGE')
+        log.debug("Scaling the size of CloudMan cluster")
+        # Enable autoscaling with sufficient cluster size limits to enable the
+        # 22 models to run in parallel
+        # TODO: examine the size of the instance and adjust AS appropriately
+        cm.enable_autoscaling(0, 7)
+    else:
+        log.debug("CloudMan already setup; didn't do anything.")
 else:
-    log.debug("CloudMan already setup; didn't do anything.")
+    log.debug("Worker job; not attempting to initialize CloudMan")
